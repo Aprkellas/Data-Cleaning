336f629ea5785e505b825271d017b27587c55390
Aprkellas <alex.kellas.relton@gmail.com>
2023-09-08 12:50:26 +0100

    datatype fixes

--- splitdata.py
+++ splitdata.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import csv
 
-df = pd.read_csv('test\cleaned_data.csv', encoding='utf-8')
+df = pd.read_csv('test/cleaned_data.csv', encoding='utf-8')
 print(df.head())
 
 features = ['PM2.5', 'PM10', 'NO2', 'CO', 'SO2', 'O3']
@@ -12,6 +12,14 @@ target = 'AQI'
 
 X = df[features]
 y = df[target]
+try:
+    X = X.astype(int)
+except Exception as e:
+    print(e)
+try:
+    y = y.astype(int)
+except Exception as e:
+    print(e)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
