cc152ae69134344a51c377ebdc81d50e5c1a8d09
Aprkellas <alex.kellas.relton@gmail.com>
2023-10-24 08:40:47 +0100

    file moving and more comments added

--- raw/cleandata.py
+++ raw/cleandata.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 
 # Load the data into a pandas DataFrame
-df = pd.read_csv("raw\Air Quality Data in India (2015 - 2020)\Air Quality Data in India (2015 - 2020)\city_day.csv")
+df = pd.read_csv("./raw/AirQualityData/city_day.csv")
 
 # Filter rows to keep only those with at least one of PM2.5 and PM10 available
 df = df.dropna(subset=['PM2.5', 'PM10'], how='all')
@@ -16,6 +16,7 @@ df = df.dropna(subset=required_pollutants, thresh=3)
 # Handle duplicate data
 df.drop_duplicates(inplace=True)
 
+# remove the useless columns/data
 columns_to_remove = [col for col in df.columns if col not in required_pollutants]
 df.drop(columns=columns_to_remove, inplace=True)
 
