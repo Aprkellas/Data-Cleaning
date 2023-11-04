6ef9d94fff20c18b1a4d24ec99bbe7187d5cb69d
Aprkellas <alex.kellas.relton@gmail.com>
2023-11-04 09:39:59 +0000

    linear regression file update

--- featureselection.py
+++ featureselection.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-import os
-import csv
+from sklearn.linear_model import LinearRegression
 
 # Load your cleaned data
 df = pd.read_csv('./cleaned_data.csv')
@@ -22,26 +20,12 @@ correlation_threshold = 0.2  # Adjust as needed
 # Identify features with strong correlations
 correlated_features = correlation_with_target[abs(correlation_with_target) > correlation_threshold].index.tolist()
 
-# Calculate feature importances using Random Forest
-X = df[features]
-y = df[target]
-
-model = RandomForestRegressor()
-model.fit(X, y)
-feature_importances = model.feature_importances_
-
-# Set an importance threshold for feature selection
-importance_threshold = 0.05  # Adjust as needed
-
-# Identify features with high importance scores
-important_features = X.columns[feature_importances > importance_threshold]
-
-# Combine all selected features
+# Combine selected features
 selected_features = correlated_features.copy()  # Start with correlated features
-selected_features.extend(important_features)   # Add important features
 
 # Create a DataFrame with the selected features and the target
 selected_data = df[selected_features + [target]]
 
 # Save the selected data to a CSV file
 selected_data.to_csv('selected_data.csv', index=False)
+
